aeb48f27432ef0c8ba3cbb5a7520de86940ce1a3
Razvan Vacaru <razvanmaycry@gmail.com>
2019-10-28 00:47:16 +0100

    improved data provider service: checking if sample file path exist in hdfs and return empty df if not so, before doing union

--- src/main/scala/com/raz/gddtwitter/service/DataProviderService.scala
+++ src/main/scala/com/raz/gddtwitter/service/DataProviderService.scala
@@ -1,7 +1,10 @@
 package com.raz.gddtwitter.service
 
+import java.sql.Timestamp
+
 import com.raz.gddtwitter.config.properties.AppProperties
 import com.raz.gddtwitter.service.SchemaConstants._
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Column, DataFrame, Dataset, SparkSession}
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,14 +15,33 @@ class DataProviderService @Autowired()(private val sparkSession: SparkSession,
                                        private val appProperties: AppProperties) extends Serializable {
 
   def getTwitterTextDf(): DataFrame = {
-    val filteredSampleDataset = readAndFilterSampleJson(appProperties.sampleHdfsPath,
-      (str: String) => str.startsWith(COORDINATES_PREFIX))
+    import sparkSession.implicits._
 
-    val filteredTwitterSampleDataset = readAndFilterSampleJson(appProperties.twitterSampleHdfsPath,
-      (str: String) => str.contains(TWITTER_TYPE_PATTERN) && !str.contains(RETWEET_PATTERN))
+    val sampleExists: Boolean = existsInHdfs(appProperties.sampleHdfsPath)
+    val twitterSampleExists: Boolean = existsInHdfs(appProperties.twitterSampleHdfsPath)
+
+    var filteredSampleDataset = sparkSession.emptyDataset[String]
+    if (sampleExists) {
+      filteredSampleDataset = readAndFilterSampleJson(appProperties.sampleHdfsPath,
+        (str: String) => str.startsWith(COORDINATES_PREFIX))
+    }
+
+    var filteredTwitterSampleDataset = sparkSession.emptyDataset[String]
+    if (twitterSampleExists) {
+      filteredTwitterSampleDataset = readAndFilterSampleJson(appProperties.twitterSampleHdfsPath,
+        (str: String) => str.contains(TWITTER_TYPE_PATTERN) && !str.contains(RETWEET_PATTERN))
+    }
 
     toTwitterCreatedAtAndTextDf(filteredTwitterSampleDataset)
       .union(toCreatedAtAndTextDf(filteredSampleDataset))
+  }
+
+  private def existsInHdfs(hdfsPath: String): Boolean = {
+    val hadoopConfig = sparkSession.sparkContext.hadoopConfiguration
+    val hdfs = FileSystem.get(hadoopConfig)
+    val exists = hdfs.exists(new Path(hdfsPath))
+
+    exists
   }
 
   private def readAndFilterSampleJson(hdfsPath: String, filterFunction: String => Boolean): Dataset[String] = {
@@ -29,29 +51,38 @@ class DataProviderService @Autowired()(private val sparkSession: SparkSession,
   }
 
   private def toCreatedAtAndTextDf(stringDataset: Dataset[String]): DataFrame = {
+    if (stringDataset.isEmpty) {
+      return emptyTwitterDf()
+    }
+
     val filterDf = sparkSession.read.json(stringDataset)
       .select(col(CREATED_AT), col(TEXT))
       .where(createdAtAndTextAreNotNullOrEmptyStringsCondition())
-      .withColumn(CREATED_AT, asTimestamp(DATE_FORMAT_SAMPLE))
+      .withColumn(CREATED_AT, to_timestamp(col(CREATED_AT), DATE_FORMAT_SAMPLE))
 
     filterDf
   }
 
   private def toTwitterCreatedAtAndTextDf(stringDataset: Dataset[String]): DataFrame = {
+    if (stringDataset.isEmpty) {
+      return emptyTwitterDf()
+    }
+
     val filterDf = sparkSession.read.json(stringDataset)
       .select(col(TWITTER_CREATED_AT), col(TWITTER_TEXT))
       .where(createdAtAndTextAreNotNullOrEmptyStringsCondition())
-      .withColumn(CREATED_AT, asTimestamp(DATE_FORMAT_TWITTER_SAMPLE))
+      .withColumn(CREATED_AT, to_timestamp(col(CREATED_AT), DATE_FORMAT_TWITTER_SAMPLE))
 
     filterDf
   }
 
-  private def asTimestamp(dateFormat: String):Column = {
-    to_timestamp(col(CREATED_AT), dateFormat)
-  }
-
   private def createdAtAndTextAreNotNullOrEmptyStringsCondition(): Column = {
     col(CREATED_AT).isNotNull && col(CREATED_AT) =!= "" && col(TEXT).isNotNull && col(TEXT) =!= ""
+  }
+
+  private def emptyTwitterDf(): DataFrame = {
+    import sparkSession.implicits._
+    Seq.empty[(Timestamp, String)].toDF(Seq(CREATED_AT, TEXT): _*)
   }
 
 }
